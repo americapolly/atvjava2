addd20e055d708ffea6b33ede5f7a0de1df7a0f1
Polly <112908864+americapolly@users.noreply.github.com>
2023-05-22 21:15:43 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,53 @@
+public class Main {
+public static void main(String[] args) {
+    int numero1 = 5;
+    int numero2 = 3;
+    boolean resultado;
+
+    resultado = numero1 == numero2;
+    System.out.println(numero1 + ", é igual ao " + numero2 + "? " + resultado);
+    resultado = numero1 != numero2;
+    System.out.println(numero1 + ", é diferente do " + numero2 + "? " + resultado);
+    resultado = numero1 > numero2;
+    System.out.println(numero1 + ", é maior que o " + numero2 + "? " + resultado);
+    resultado = numero1 < numero2;
+    System.out.println(numero1 + ", é menor que o " + numero2 + "? "  + resultado);
+    resultado = numero1 >= numero2;
+    System.out.println(numero1 + ", é maior ou igual ao " + numero2 + "? " + resultado);
+    resultado = numero1 <= numero2;
+    System.out.println(numero1 + ", é menor ou igual ao " + numero2 + "? " + resultado);
+
+    numero1 = 5;
+    numero2 = 5;
+
+    resultado = numero1 == numero2;
+    System.out.println(numero1 + ", é igual ao " + numero2 + "? " + resultado);
+    resultado = numero1 != numero2;
+    System.out.println(numero1 + ", é diferente do " + numero2 + "? " + resultado);
+    resultado = numero1 > numero2;
+    System.out.println(numero1 + ", é maior que o " + numero2 + "? " + resultado);
+    resultado = numero1 < numero2;
+    System.out.println(numero1 + ", é menor que o " + numero2 + "? " + resultado);
+    resultado = numero1 >= numero2;
+    System.out.println(numero1 + ", é maior ou igual ao " + numero2 + "? " + resultado);
+    resultado = numero1 <= numero2;
+    System.out.println(numero1 + ", é menor ou igual ao " + numero2 + "? " + resultado);
+
+    numero1 = 5;
+    numero2 = 8;
+
+    resultado = numero1 == numero2;
+    System.out.println(numero1 + ", é igual ao " + numero2 + "? " + resultado);
+    resultado = numero1 != numero2;
+    System.out.println(numero1 + ", é diferente do " + numero2 + "? " + resultado);
+    resultado = numero1 > numero2;
+    System.out.println(numero1 + ", é maior que o " + numero2 + "? " + resultado);
+    resultado = numero1 < numero2;
+    System.out.println(numero1 + ", é menor que o " + numero2 + "? " + resultado);
+    resultado = numero1 >= numero2;
+    System.out.println(numero1 + ", é maior ou igual ao " + numero2 + "? " + resultado);
+    resultado = numero1 <= numero2;
+    System.out.println(numero1 + ", é menor ou igual ao " + numero2 + "? " + resultado);
+
+}
+}
